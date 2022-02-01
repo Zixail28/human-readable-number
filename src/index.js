e7f922d8d611c84d3784787c702afc75ec6ef017
Zixail28 <bezgubov.04@yandex.ru>
2022-02-01 18:24:06 +0300

    feat: add toReadable function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function toReadable (number) {
-  
+  const strNumEdin = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+  const strNumEd = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+  const strNumTT = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+  const strNumDes = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+  const getStr = number.toString()
+  if(getStr.length === 1){
+  let num = Number(getStr)
+  return `${strNumEdin[num]}`
+  } else if (getStr.length === 2){
+      if(Number(getStr[0]) === 1){
+        let num = Number(getStr[1])
+        return `${strNumTT[num]}`
+      } return `${strNumDes[Number(getStr[0])]}${checkdes(getStr)}${strNumEd[Number(getStr[1])]}`
+  } else if(getStr.length === 3){
+      let result = ''
+      result = `${strNumEd[Number(getStr[0])]} hundred`
+      if(getStr[1] === '0' && getStr[2] === '0'){
+          return result
+      } else if(getStr[1] === '0' && getStr[2] !== '0') {
+          result = `${result} ${strNumEd[Number(getStr[2])]}`
+      } else if(getStr[1] !== '0' && getStr[2] === '0'){
+        result = `${result} ${strNumDes[Number(getStr[1])]}`
+      } else if(getStr[1] == '1' && getStr[2] !== '0'){
+        result = `${result} ${strNumTT[Number(getStr[2])]}`
+      } else if(getStr[1] !== '0' && getStr[2] !== '0'){
+          result = `${result} ${strNumDes[Number(getStr[1])]} ${strNumEd[Number(getStr[2])]}`
+      }
+      return result
+  }
+
+  function checkdes(getStr){
+      if(getStr[1] === '0'){return ''} return ' '
+  }
 }
